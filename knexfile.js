0394ad9ade10df7af14114159ca8d97a78d4d2b2
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 20:16:20 -0800

    database created and connected, tables creates

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://thdbmayh:SyK27KShvPnAEpOYHYIy-FAj7t6DGobu@bubble.db.elephantsql.com/thdbmayh",
 } = process.env;
 
 module.exports = {
