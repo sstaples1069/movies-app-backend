534131e5ac3b9881e5526c2fcd1a2529cd13e3d1
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 20:51:10 -0800

    added movies route to app

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const errorHandler = require("../errors/errorHandler")
+const errorHandler = require("../errors/errorHandler");
 const notFound = require("../errors/notFound");
+const moviesRouter = require("../movies/movies.router");
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/movies", moviesRouter);
 
 app.use(notFound);
 app.use(errorHandler)
