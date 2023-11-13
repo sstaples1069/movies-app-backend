63b79b3c2a6a19b3c71d84f524709ebf7fbc5f4d
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 19:43:36 -0800

    added error handling and cors top level

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const errorHandler = require("../errors/errorHandler")
+const notFound = require("../errors/notFound");
+
+app.use(cors());
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler)
 
 module.exports = app;
